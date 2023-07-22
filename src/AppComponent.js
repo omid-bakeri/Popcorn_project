3cf12edc1492337fb60f3f73c1182cb5093241f2
Robert.minia <omid-bakeri>
2023-07-22 14:48:41 +0430

    file updated react updated

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -62,26 +62,26 @@ export default function AppComponent() {
   // } , [])
 
   //async await
+  const [watched, setWatched] = useState([]);
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedMovieID, setSelectedMovieID] = useState();
 
-  let rand = Math.floor(Math.random() * 5) + 1;
-  console.log(rand);
+  // let rand = Math.floor(Math.random() * 5) + 1;
+
+  // console.log(rand);
   function handleMovieSelectedDetails(id) {
     setSelectedMovieID(id);
     // console.log("id : " , movie);
   }
 
-  // useEffect(function () {
-  //     console.log("A");
-  // }, []);
-  // useEffect(function () {
-  //     console.log("B");
-  // });
-  // console.log("C");
+  function handleAddWatched(movie) {
+    setWatched((watched) => [...watched, movie]);
+    console.log(watched);
+    // console.log(watched[0].Title);
+  }
 
   useEffect(() => {
     // setLoading(false)
@@ -117,7 +117,10 @@ export default function AppComponent() {
       <NavBar movies={movies} query={query} setQuery={setQuery}>
         <Results movies={movies} />
       </NavBar>
-      <Main handleMovieSelectedDetails={handleMovieSelectedDetails}>
+      <Main
+        handleMovieSelectedDetails={handleMovieSelectedDetails}
+        handleAddWatched={handleAddWatched}
+      >
         <MovieSearchResults>
           {error && <ErrorNet error={error} />}
           {!loading && !error && (
@@ -130,12 +133,13 @@ export default function AppComponent() {
         </MovieSearchResults>
         {selectedMovieID ? (
           <MovieSelected
+            handleAddWatched={handleAddWatched}
             selectedMovieID={selectedMovieID}
             handleCloseTab={handleCloseTab}
             handleMovieSelectedDetails={handleMovieSelectedDetails}
           />
         ) : (
-          <MovieYourWatchedInformation />
+          <MovieYourWatchedInformation handleAddWatched={watched} />
         )}
       </Main>
     </>
@@ -201,7 +205,7 @@ function Results({ movies }) {
   );
 }
 // main
-function Main({ children, handleMovieSelectedDetails }) {
+function Main({ children, handleMovieSelectedDetails, OnFavoriteMovie }) {
   return (
     <>
       <main className="main">{children}</main>
@@ -228,7 +232,7 @@ function MovieSearchResults({ children }) {
 }
 
 // MovieList
-function MovieList({ movies, handleMovieSelectedDetails }) {
+function MovieList({ movies, handleMovieSelectedDetails, handleAddWatched }) {
   return (
     <ul className="list">
       {movies?.map((movie) => (
@@ -253,7 +257,7 @@ function MovieList({ movies, handleMovieSelectedDetails }) {
 }
 
 // movie you watched information
-function MovieYourWatchedInformation() {
+function MovieYourWatchedInformation(handleAddWatched) {
   const [isOpen2, setIsOpen2] = useState(true);
   return (
     <div className="box overflow-x-hidden">
@@ -263,7 +267,7 @@ function MovieYourWatchedInformation() {
       >
         {isOpen2 ? "–" : "+"}
       </button>
-      {isOpen2 && <MovieWatchedList />}
+      {isOpen2 && <MovieWatchedList handleAddWatched={handleAddWatched} />}
     </div>
   );
 }
@@ -272,6 +276,7 @@ function MovieSelected({
   selectedMovieID,
   handleCloseTab,
   handleMovieSelectedDetails,
+  handleAddWatched,
 }) {
   return (
     <>
@@ -324,17 +329,22 @@ function MovieSelected({
           <MovieDetails
             selectedMovieID={selectedMovieID}
             handleMovieSelectedDetails={handleMovieSelectedDetails}
+            // eslint-disable-next-line no-undef
+            handleAddWatched={handleAddWatched}
           />
-          {/* {selectedMovieID} */}
         </div>
       </div>
     </>
   );
 }
 //movieDetails
-function MovieDetails({ selectedMovieID, handleMovieSelectedDetails }) {
+function MovieDetails({
+  selectedMovieID,
+  handleMovieSelectedDetails,
+  handleAddWatched,
+}) {
   // {console.log("id=" , selectedMovieID)}
-  console.log(selectedMovieID);
+  // console.log(selectedMovieID);
   const [movieDetails, setMovieDetails] = useState({});
 
   const {
@@ -355,9 +365,20 @@ function MovieDetails({ selectedMovieID, handleMovieSelectedDetails }) {
     imdbRating: imdbRating,
   } = movieDetails;
 
-  console.log(movieDetails);
+  // console.log(movieDetails);
 
   // console.log(Title , Year);
+  function handleAdd() {
+    const new_watched_movie = {
+      imdbID: selectedMovieID,
+      Title,
+      Year,
+      Poster,
+      imdbRating: Number(imdbRating),
+      Runtime: Number(Runtime.split(" ").at(0)),
+    };
+    handleAddWatched(new_watched_movie);
+  }
   useEffect(
     function ApiDetails() {
       async function ApiDetailsFunction() {
@@ -365,7 +386,7 @@ function MovieDetails({ selectedMovieID, handleMovieSelectedDetails }) {
           `https://www.omdbapi.com/?apikey=${key}&i=${selectedMovieID}`
         );
 
-        console.log("------------------------------------");
+        // console.log("------------------------------------");
         const res = await fetchDetails.json();
         // console.log(res);
         setMovieDetails(res);
@@ -419,7 +440,7 @@ function MovieDetails({ selectedMovieID, handleMovieSelectedDetails }) {
           paddingTop: "7px",
         }}
       >
-          {/*<div className="fa fa-man"></div>*/}
+        {/*<div className="fa fa-man"></div>*/}
         <div className="movieDetailsClass">Actors</div>
         <div className="movieDetailsClassData">: {Actors}</div>
       </div>
@@ -431,7 +452,7 @@ function MovieDetails({ selectedMovieID, handleMovieSelectedDetails }) {
           paddingTop: "7px",
         }}
       >
-          {/*<i className="fa fa-time"></i>*/}
+        {/*<i className="fa fa-time"></i>*/}
         <div className="movieDetailsClass">Time</div>
         <div className="movieDetailsClassData">: {Runtime}</div>
       </div>
@@ -506,60 +527,100 @@ function MovieDetails({ selectedMovieID, handleMovieSelectedDetails }) {
           {Plot}
         </div>
       </div>
+      <button onClick={handleAdd} className="btn-favorite">
+        Add to Favorite
+      </button>
     </>
   );
 }
 // movieWatchedList
-function MovieWatchedList() {
-  const [watched, setWatched] = useState(tempWatchedData);
+function MovieWatchedList({ handleAddWatched }) {
+  // console.log("*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+  // console.log(handleAddWatched.handleAddWatched.at(0).Title);
+  // console.log("*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+  let Favorite_label = [];
+  Favorite_label = handleAddWatched.handleAddWatched;
 
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  // const [fav, setFav] = useState([]);
+  // setFav(handleAddWatched);
+
+  // const {
+  //   Title: Title,
+  //   Actors: Actors,
+  //   Awards: Awards,
+  //   BoxOffice: BoxOffice,
+  //   Country: Country,
+  //   DVD: DVD,
+  //   Director: Director,
+  //   Genre: Genre,
+  //   Language: Language,
+  //   Plot: Plot,
+  //   Poster: Poster,
+  //   Released: Released,
+  //   Runtime: Runtime,
+  //   Year: Year,
+  //   imdbRating: imdbRating,
+  // } = watched;
+
+  // console.log("-*-*-*-*-*-*-*-*-*-*-*-");
+  // console.log(OnFavoriteMovie.Title);
+  // function handleDataFavoriteMovie() {
+  //   setWatched(OnFavoriteMovie);
+  //  }
+
+  // const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  // const avgUserRating = average(watched.map((movie) => movie.userRating));
+  // const avgRuntime = average(watched.map((movie) => movie.runtime));
   return (
     <>
-      <div className="summary">
-        <h2>Movies you watched</h2>
-        <div>
-          <p>
-            <span>#️⃣</span>
-            <span>{watched.length} movies</span>
-          </p>
-          <p>
-            <span>⭐️</span>
-            <span>{avgImdbRating}</span>
-          </p>
-          <p>
-            <span>🌟</span>
-            <span>{avgUserRating}</span>
-          </p>
-          <p>
-            <span>⏳</span>
-            <span>{avgRuntime} min</span>
-          </p>
-        </div>
-      </div>
-
       <ul className="list">
-        {watched.map((movie) => (
-          <li key={movie.imdbID}>
-            <img src={movie.Poster} alt={`${movie.Title} poster`} />
-            <h3>{movie.Title}</h3>
+        <div className="favorite-title-movie">Your Favorite Movies</div>
+        {Favorite_label.map((movie) => (
+          <div className="img-close-btn" style={{ padding: "4px" }}>
             <div>
-              <p>
-                <span>⭐️</span>
-                <span>{movie.imdbRating}</span>
-              </p>
-              <p>
-                <span>🌟</span>
-                <span>{movie.userRating}</span>
-              </p>
-              <p>
-                <span>⏳</span>
-                <span>{movie.runtime} min</span>
-              </p>
+              <div
+                className="root-movie-favorite"
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  gap: "10px",
+                }}
+              >
+                <img src={movie.Poster} alt={movie.Title} />
+                <div
+                  style={{
+                    display: "flex",
+                    // gap : "2",
+                    flexDirection: "column",
+                    maxWidth: "310px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <p className="title_movie_favorite">{movie.Title}</p>
+                  <div className="" style={{ width: "100%" }}>
+                    <p style={{ display: "flex", gap: "2px" }}>
+                      <span style={{ fontSize: "1.6rem" }}>📅</span>
+                      <span style={{ fontSize: "1.6rem" }}>{movie.Year}</span>
+                    </p>
+                    <p style={{ display: "flex", gap: "2px" }}>
+                      <span style={{ fontSize: "1.6rem" }}>⌚</span>
+                      <span style={{ fontSize: "1.6rem" }}>
+                        {movie.Runtime} min
+                      </span>
+                    </p>
+                    <p style={{ display: "flex", gap: "2px" }}>
+                      <span style={{ fontSize: "1.6rem" }}>⭐</span>
+                      <span style={{ fontSize: "1.6rem" }}>
+                        {movie.imdbRating}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-          </li>
+            <p class="closeMovie">x</p>
+          </div>
         ))}
       </ul>
     </>
